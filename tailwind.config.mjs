f8b2c1ba64ae34eb55ef2864faea2f9c3b298072
XMrtN <sepulvedamartin072@gmail.com>
2024-02-24 22:32:52 -0300

    added @midudev/tailwind-animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animations from "@midudev/tailwind-animations";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -57,5 +58,6 @@ export default {
         ":merge(.group):active &",
       ]);
     },
+    animations,
   ],
 };
