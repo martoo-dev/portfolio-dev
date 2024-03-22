306bba1312ec6d1555c698c9023a3815b02322fc
XMrtN <sepulvedamartin072@gmail.com>
2024-03-21 22:04:20 -0300

    updated darkmode to tailwind 3.4.1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ import animations from "@midudev/tailwind-animations";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
+  darkMode: "selector",
   safelist: [
     {
       pattern:
